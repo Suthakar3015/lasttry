d97b631f7cb5e7d1a6a5ac17a30a2ad24b02bafd
unknown <suthakar.kamu@gmail.com>
2022-08-11 08:44:00 +0530

    new java project

---
+++ src/org/testjava/Test.java
@@ -0,0 +1,12 @@
+package org.testjava;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+	}
+
+}
